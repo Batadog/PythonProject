eeadf27ede3e307bfe5461ce1a87b51cb31b4ecf
Chinali <36908316+Batadog@users.noreply.github.com>
2018-08-28 11:15:15 +0800

    first

---
+++ venv/Include/basisGrammar.py
@@ -0,0 +1,27 @@
+#行和缩进
+#一行显示多条语句
+print("hello");print("world")
+#一条语句显示多行，用“\” 或者{}（）
+print("hello\
+      world\
+      ")
+
+Str=['hello',
+     'world','I']
+print(Str)
+# 缩进
+if False:
+     print("answer")
+     print("true")
+else:
+     print("answer")
+     print("false")
+# .............
+#注释
+# 多行注释： 三单引号 或者 三双引号
+"""
+多行注释1
+"""
+'''
+多行注释2
+'''
